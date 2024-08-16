c706bd1f768ee96302df1543e3ed6b185bfcdcf0
Mukesh <mukesh@pratikshat.com>
2024-08-16 16:59:14 +0530

    component create

---
+++ src/app/layout/agent/agent-dashboard/agent-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './agent-dashboard.component.html',
+  styleUrl: './agent-dashboard.component.scss'
+})
+export class AgentDashboardComponent {
+
+}
---
+++ src/app/layout/user/user-rights/user-rights.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-rights',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-rights.component.html',
+  styleUrl: './user-rights.component.scss'
+})
+export class UserRightsComponent {
+
+}
